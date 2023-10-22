05c30b9d56472871a17bb45df1c5c1fee9b814c7
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-10-21 22:46:08 -0500

    update description and default height

--- src/routes/(default)/+layout.svelte
+++ src/routes/(default)/+layout.svelte
@@ -5,15 +5,15 @@
 <div class="h-2 w-full bg-gradient-to-r from-indigo-600 to-fuchsia-400" />
 
 <ContentContainer>
-    <div class="space-y-1.5 pb-12 pt-8 lg:pt-12">
+    <div class="space-y-1.5 pb-6 pt-8 lg:pt-12">
         <h1 class="font-heading text-2xl font-semibold md:text-5xl lg:text-7xl">SRM Executables</h1>
         <p class="max-w-2xl text-muted-foreground">
-            I do not know what this does, but I am sure it is important. <a
+            A utility that calculates the synthesis rates of specific proteins. Created by <a
                 class="font-medium underline underline-offset-4"
-                href="https://github.com/rgsadygov/SRM_executables"
+                href="https://github.com/ericwang401"
                 target="_blank"
             >
-                SRM Executables binaries
+                Eric Wang
             </a>
         </p>
     </div>
